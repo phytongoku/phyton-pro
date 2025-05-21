7e685c9b48d52ad9fe5e332c91d1832a65e3b32a
phytongoku <miguelisai862@gmail.com>
2025-05-20 19:36:13 -0600

    Create M1L1.py

---
+++ M1L1.py
@@ -0,0 +1,19 @@
+for i in range(5):
+    word = input("hola","Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    meme_dict = {
+                "CRINGE": "Algo excepcionalmente raro o embarazoso",
+                "LOL": "Una respuesta común a algo gracioso",
+                "ROFL - una respuesta a una broma",
+                "SHEESH - ligera desaprobación",
+                "CREEPY - aterrador, siniestro",
+                "AGGRO - ponerse agresivo/enojado"
+                }
+   
+    3. ¿Cómo procesaremos la solicitud del usuario y le devolveremos la explicación?
+    
+    if word in meme_dict.keys():
+        # ¿Qué debemos hacer si se encuentra la palabra?
+    else:
+        # ¿Qué hacer si no se encuentra la palabra?
+     
+    
